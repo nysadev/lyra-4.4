48ef7e72514c7bd08e3765dff96d797e03542c59
Peter Zijlstra <peterz@infradead.org>
2016-09-19 12:57:53 +0200

    sched/core: Avoid _cond_resched() for PREEMPT=y
    On fully preemptible kernels _cond_resched() is pointless, so avoid
    emitting any code for it.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mikulas Patocka <mpatocka@redhat.com>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4965,6 +4965,7 @@ SYSCALL_DEFINE0(sched_yield)
 	return 0;
 }
 
+#ifndef CONFIG_PREEMPT
 int __sched _cond_resched(void)
 {
 	if (should_resched(0)) {
@@ -4974,6 +4975,7 @@ int __sched _cond_resched(void)
 	return 0;
 }
 EXPORT_SYMBOL(_cond_resched);
+#endif
 
 /*
  * __cond_resched_lock() - if a reschedule is pending, drop the given lock,
