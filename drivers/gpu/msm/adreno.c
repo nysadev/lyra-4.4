6779a7eca4486ed5406c8c0a4459aed31b783690
Carter Cooper <ccooper@codeaurora.org>
2017-03-15 15:01:09 -0600

    msm: kgsl: Move set_secured_mode into adreno
    This is some common code, so move it to the base and make sure
    all common registers are set/initialized properly.
    
    Change-Id: Iedc83611d7c6801ff3906a44fc6cf6b1b216d440
    Signed-off-by: Carter Cooper <ccooper@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>
    Signed-off-by: Jago Gardiner (nysadev) <jagogardiner@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -1165,7 +1165,7 @@ static int adreno_switch_to_unsecure_mode(struct adreno_device *adreno_dev,
 
 	ret = adreno_ringbuffer_submit_spin(rb, NULL, 2000);
 	if (ret)
-		adreno_spin_idle_debug(adreno_dev,
+		spin_idle_debug(adreno_dev,
 				"Switch to unsecure failed to idle\n");
 
 	return ret;
@@ -1187,7 +1187,7 @@ int adreno_set_unsecured_mode(struct adreno_device *adreno_dev,
 	}
 
 	/* GPU comes up in secured mode, make it unsecured by default */
-	if (adreno_dev->zap_loaded)
+	if (ADRENO_FEATURE(adreno_dev, ADRENO_CONTENT_PROTECTION))
 		ret = adreno_switch_to_unsecure_mode(adreno_dev, rb);
 	else
 		adreno_writereg(adreno_dev,
